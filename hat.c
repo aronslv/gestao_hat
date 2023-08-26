4b5b9b8c636c92e233c1e09e80f0780f7152513b
Aron Silva <aronsilvagm@gmail.com>
2023-08-26 14:22:09 -0300

    Criação de CRUDS de outros módulos

--- hat.c
+++ hat.c
@@ -10,8 +10,20 @@ void tela_pes_rev(void);
 void tela_edit_rev(void);
 void tela_exc_rev(void);
 void tela_funcionarios(void);
+void tela_cad_func(void);
+void tela_pes_func(void);
+void tela_edit_func(void);
+void tela_exc_func(void);
 void tela_fornecedoras(void);
+void tela_cad_forn(void);
+void tela_pes_forn(void);
+void tela_edit_forn(void);
+void tela_exc_forn(void);
 void tela_produtos(void);
+void tela_cad_prods(void);
+void tela_pes_prods(void);
+void tela_edit_prods(void);
+void tela_exc_prods(void);
 
 //programa main
 int main(void) {
@@ -23,8 +35,20 @@ int main(void) {
     tela_edit_rev();
     tela_exc_rev();
     tela_funcionarios();
+    tela_cad_func();
+    tela_pes_func();
+    tela_edit_func();
+    tela_exc_func();
     tela_fornecedoras();
+    tela_cad_forn();
+    tela_pes_forn();
+    tela_edit_forn();
+    tela_exc_forn();
     tela_produtos();
+    tela_cad_prods();
+    tela_pes_prods();
+    tela_edit_prods();
+    tela_exc_prods();
     return 0;
 }
 
@@ -109,7 +133,7 @@ void tela_cad_rev(void) {
     printf("§              CNPJ:                                                          §\n");
     printf("§              Nome do estabelecimento:                                       §\n");
     printf("§              Endereço do estabelecimento:                                   §\n");
-    printf("§              Nome do proprietário(a)                                        §\n");
+    printf("§              Nome do proprietário(a):                                       §\n");
     printf("§              Celular (apenas números):                                      §\n");
     printf("§                                                                             §\n");
     printf("§                                                                             §\n");
@@ -157,6 +181,194 @@ void tela_exc_rev(void) {
     printf("-------------------------------------------------------------------------------\n");
     printf("§                                                                             §\n");
     printf("§              < < < < < < < Exclusão - Revendedoras > > > > > > >            §\n");
+    printf("§                                                                             §\n");
+    printf("§              Informe o CNPJ para excluir do sistema:                        §\n");
+    printf("§                                                                             §\n");
+    printf("§                                                                             §\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar(); 
+}
+
+void tela_funcionarios(void) {
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("§                                                                             §\n");
+    printf("§              < < < < < < < < Funcionários > > > > > > > >                   §\n");
+    printf("§                                                                             §\n");
+    printf("§              1. Cadastro de Funcionários                                    §\n");
+    printf("§              2. Pesquisar Funcionários                                      §\n");
+    printf("§              3. Editar Dados de Funcionários                                §\n");
+    printf("§              4. Excluir um Funcionário do Sistema                           §\n");
+    printf("§              0. Retornar ao Menu Principal                                  §\n");
+    printf("§                                                                             §\n");
+    printf("§              Digite o nº da opção desejada: ");
+    scanf("%c", &op);
+    getchar();
+    printf("§                                                                             §\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void tela_cad_func(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("§                                                                             §\n");
+    printf("§              < < < < < < < Cadastro - Funcionários > > > > > > >            §\n");
+    printf("§                                                                             §\n");
+    printf("§              Digite as informações a seguir:                                §\n");
+    printf("§                                                                             §\n");
+    printf("§              CPF:                                                           §\n");
+    printf("§              Nome completo:                                                 §\n");
+    printf("§              Profissão:                                                     §\n");
+    printf("§              Celular (apenas números):                                      §\n");
+    printf("§                                                                             §\n");
+    printf("§                                                                             §\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void tela_pes_func(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("§                                                                             §\n");
+    printf("§              < < < < < < < Pesquisa - Funcionários > > > > > > >            §\n");
+    printf("§                                                                             §\n");
+    printf("§              Informe o CPF que deseja pesquisar:                            §\n");
+    printf("§                                                                             §\n");
+    printf("§                                                                             §\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar(); 
+}
+
+void tela_edit_func(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("§                                                                             §\n");
+    printf("§              < < < < < < < Edição - Funcionários > > > > > > >              §\n");
+    printf("§                                                                             §\n");
+    printf("§              Informe o CPF para edição de dados:                            §\n");
+    printf("§                                                                             §\n");
+    printf("§                                                                             §\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar(); 
+}
+
+void tela_exc_func(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("§                                                                             §\n");
+    printf("§              < < < < < < < Exclusão - Funcionários > > > > > > >            §\n");
+    printf("§                                                                             §\n");
+    printf("§              Informe o CPF para excluir do sistema:                         §\n");
+    printf("§                                                                             §\n");
+    printf("§                                                                             §\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar(); 
+}
+
+void tela_fornecedoras(void) {
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("§                                                                             §\n");
+    printf("§              < < < < < < < < Fornecedoras > > > > > > > >                   §\n");
+    printf("§                                                                             §\n");
+    printf("§              1. Cadastro de Fornecedoras                                    §\n");
+    printf("§              2. Pesquisar Fornecedoras                                      §\n");
+    printf("§              3. Editar Dados de Fornecedoras                                §\n");
+    printf("§              4. Excluir uma Fornecedora do Sistema                          §\n");
+    printf("§              0. Retornar ao Menu Principal                                  §\n");
+    printf("§                                                                             §\n");
+    printf("§              Digite o nº da opção desejada: ");
+    scanf("%c", &op);
+    getchar();
+    printf("§                                                                             §\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void tela_cad_forn(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("§                                                                             §\n");
+    printf("§              < < < < < < < Cadastro - Fornecedoras > > > > > > >            §\n");
+    printf("§                                                                             §\n");
+    printf("§              Digite as informações a seguir:                                §\n");
+    printf("§                                                                             §\n");
+    printf("§              CNPJ:                                                          §\n");
+    printf("§              Nome do estabelecimento:                                       §\n");
+    printf("§              Endereço do estabelecimento:                                   §\n");
+    printf("§              Matéria-prima fornecida:                                       §\n");
+    printf("§              Nome do proprietário(a):                                       §\n");
+    printf("§              Celular (apenas números):                                      §\n");
+    printf("§                                                                             §\n");
+    printf("§                                                                             §\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void tela_pes_forn(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("§                                                                             §\n");
+    printf("§              < < < < < < < Pesquisa - Fornecedoras > > > > > > >            §\n");
+    printf("§                                                                             §\n");
+    printf("§              Informe o CNPJ que deseja pesquisar:                           §\n");
+    printf("§                                                                             §\n");
+    printf("§                                                                             §\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar(); 
+}
+
+void tela_edit_forn(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("§                                                                             §\n");
+    printf("§              < < < < < < < Edição - Fornecedoras > > > > > > >              §\n");
+    printf("§                                                                             §\n");
+    printf("§              Informe o CNPJ para edição de dados:                           §\n");
+    printf("§                                                                             §\n");
+    printf("§                                                                             §\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar(); 
+}
+
+void tela_exc_forn(void) {
+    system("clear||cls");
+    printf("\n");
+    printf("-------------------------------------------------------------------------------\n");
+    printf("§                                                                             §\n");
+    printf("§              < < < < < < < Exclusão - Fornecedoras > > > > > > >            §\n");
     printf("§                                                                             §\n");
     printf("§              Informe o CNPJ para excluir do sistema:                        §\n");
     printf("§                                                                             §\n");
