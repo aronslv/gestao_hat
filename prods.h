f9f42876be8824f17fdd18fe2f2146c994febddf
Aron Silva <aronsilvagm@gmail.com>
2023-09-19 19:59:34 -0300

    Criação do arquivo "prods.h"

---
+++ prods.h
@@ -0,0 +1,13 @@
+// funções prods
+
+#ifndef PRODS_H
+#define PRODS_H
+
+void modulo_prods(void);
+char tela_produtos(void);
+void tela_cad_prods(void);
+void tela_pes_prods(void);
+void tela_edit_prods(void);
+void tela_exc_prods(void);
+
+#endif
