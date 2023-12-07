4add3e689829868c3e30be5c5f048fdb97da0266
Aron Silva <aronsilvagm@gmail.com>
2023-12-07 13:20:00 -0300

    Validação de CPFs de testes

--- valid.c
+++ valid.c
@@ -148,11 +148,11 @@ int valid_cpf (char* cpf) {
     if (strlen(cpf) != 11) {
         return false;
     }
-    else if(/*(strcmp (cpf,"00000000000") == 0) ||(strcmp (cpf,"11111111111") == 0) || (strcmp (cpf,"22222222222") == 0) ||*/
-            // (strcmp (cpf,"33333333333") == 0) || (strcmp (cpf,"44444444444") == 0) || (strcmp (cpf,"55555555555") == 0) ||
-            // (strcmp (cpf,"66666666666") == 0) || (strcmp (cpf,"77777777777") == 0) || (strcmp (cpf,"88888888888") == 0) ||
-            /*(strcmp (cpf,"99999999999") == 0) || */(strcmp (cpf,"12345678909") == 0)) {
-        return false; //se o CPF estiver dentre os mencionados acima, ele é considerado cpf de teste.
+    else if((strcmp (cpf,"00000000000") == 0) ||(strcmp (cpf,"11111111111") == 0) || (strcmp (cpf,"22222222222") == 0) ||
+            (strcmp (cpf,"33333333333") == 0) || (strcmp (cpf,"44444444444") == 0) || (strcmp (cpf,"55555555555") == 0) ||
+            (strcmp (cpf,"66666666666") == 0) || (strcmp (cpf,"77777777777") == 0) || (strcmp (cpf,"88888888888") == 0) ||
+            (strcmp (cpf,"99999999999") == 0) || (strcmp (cpf,"12345678909") == 0)) {
+        return true; //se o CPF estiver dentre os mencionados acima, ele é considerado cpf de teste.
     }
     else {
         //digito 1
